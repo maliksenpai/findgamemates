2eef8999387ed39245cd70ad69911db566905ee1
maliksenpai <cramixzx@hotmail.com>
2022-01-13 19:06:06 +0300

    - added support for multiplatform in login screen

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -5,11 +5,13 @@ import 'package:findgamemates/screen/main_screen.dart';
 import 'package:findgamemates/theme_data.dart';
 import 'package:findgamemates/utils/dialog_utils.dart';
 import 'package:findgamemates/utils/regex_util.dart';
+import 'package:findgamemates/view/login_text_area.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:loading_indicator/loading_indicator.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -30,13 +32,22 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   void initState() {
     super.initState();
+
   }
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(title: const Text("Giriş Ekranı"), elevation: 0,),
+    return PlatformScaffold(
+      appBar: PlatformAppBar(
+        title: const Text("Giriş Ekranı"),
+        material: (_, __) => MaterialAppBarData(
+          elevation: 0
+        ),
+        cupertino: (_, __) => CupertinoNavigationBarData(
+
+        ),
+      ),
       body: SafeArea(
         child: Center(
           child: Form(
@@ -56,103 +67,28 @@ class _LoginScreenState extends State<LoginScreen> {
                   width: MediaQuery.of(context).size.width * 0.5,
                   child: FittedBox(
                     fit: BoxFit.contain,
-                    child: Text(
+                    child: PlatformText(
                       "Aslan Ağızı Hanı",
                       style: GoogleFonts.lobster(),
                     ),
                   ),
                 ),
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.7,
-                      child: TextFormField(
-                        controller: mailController,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide(color: CustomThemeData.primaryColor),
-                          ),
-                          prefixIcon: Icon(Icons.person),
-                          labelText: "Mail Adresi",
-                        ),
-                        keyboardType: TextInputType.emailAddress,
-                        validator: (value){
-                          if(value == null || value.isEmpty){
-                            return "Mail adresini boş bırakmayın";
-                          }else{
-                            if(GetUtils.isEmail(value)){
-                              return null;
-                            }else{
-                              return "Lütfen geçerli bir mail adresi giriniz";
-                            }
-                          }
-                        },
-                      ),
-                    ),
-                    const SizedBox(height: 30,),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.7,
-                      child: TextFormField(
-                        controller: passController,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20),
-                            borderSide: BorderSide(color: CustomThemeData.primaryColor)
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide(color: CustomThemeData.primaryColor),
-                          ),
-                          labelText: "Şifre",
-                          prefixIcon: Icon(Icons.security),
-                          suffixIcon: IconButton(
-                            icon: Icon(
-                              _passwordVisible
-                                  ? Icons.visibility
-                                  : Icons.visibility_off,
-                              color: CustomThemeData.accentColor,
-                            ),
-                            onPressed: () {
-                              setState(() {
-                                _passwordVisible = !_passwordVisible;
-                              });
-                            },
-                          ),
-                        ),
-                        autocorrect: false,
-                        obscureText: !_passwordVisible,
-                        enableSuggestions: false,
-                        validator: (value){
-                          if(value == null || value.isEmpty){
-                            return "Şifreyi boş bırakmayın";
-                          }else{
-                            if(RegexUtil.passwordRegex.hasMatch(value)){
-                              return null;
-                            }else{
-                              return "Şifreniz yeterince güçlü değil";
-                            }
-
-                          }
-                        },
-                      ),
-                    ),
-                  ],
+                LoginTextArea(
+                    mailController: mailController,
+                    passwordController: passController,
+                    passwordVisible: _passwordVisible,
                 ),
                 Column(
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    ElevatedButton(
+                    PlatformElevatedButton(
                       child: const Text("Giriş Yap"),
                       onPressed: () => attemptLogin(),
                     ),
                     SizedBox(height: 20,),
                     SizedBox(
                       width: MediaQuery.of(context).size.width * 0.7,
-                      child: Text(
+                      child: PlatformText(
                         "Eğer hesabınız yoksa otomatik olarak kayıt ekranına yönlendirileceksiniz",
                         style: TextStyle(fontWeight: FontWeight.bold, color: CustomThemeData.primaryColor),
                       ),
@@ -175,6 +111,7 @@ class _LoginScreenState extends State<LoginScreen> {
         ),
       ),
     );
+
   }
 
   Future<void> attemptLogin() async{
@@ -203,4 +140,6 @@ class _LoginScreenState extends State<LoginScreen> {
       Navigator.of(Get.overlayContext!).pop();
     }
   }
+
+
 }
---
+++ lib/view/login_text_area.dart
@@ -0,0 +1,133 @@
+import 'package:findgamemates/theme_data.dart';
+import 'package:findgamemates/utils/regex_util.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:get/get.dart';
+
+class LoginTextArea extends StatefulWidget {
+  TextEditingController mailController;
+  TextEditingController passwordController;
+  bool passwordVisible;
+
+  LoginTextArea({required this.mailController, required this.passwordController, required this.passwordVisible});
+
+  @override
+  _LoginTextAreaState createState() => _LoginTextAreaState();
+}
+
+class _LoginTextAreaState extends State<LoginTextArea> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        SizedBox(
+          width: MediaQuery.of(context).size.width * 0.7,
+          child: PlatformTextFormField(
+            controller: widget.mailController,
+            material: (_, __) => MaterialTextFormFieldData(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: CustomThemeData.primaryColor),
+                ),
+                prefixIcon: Icon(Icons.person),
+                labelText: "Mail Adresi",
+              ),
+            ),
+            cupertino: (_, __) => CupertinoTextFormFieldData(
+              decoration: BoxDecoration(
+                border: Border.all(color: CustomThemeData.primaryColor,),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              prefix: Icon(Icons.person),
+            ),
+            validator: (value){
+              if(value == null || value.isEmpty){
+                return "Mail adresini boş bırakmayın";
+              }else{
+                if(GetUtils.isEmail(value)){
+                  return null;
+                }else{
+                  return "Lütfen geçerli bir mail adresi giriniz";
+                }
+              }
+            },
+            keyboardType: TextInputType.emailAddress,
+
+          ),
+        ),
+        const SizedBox(height: 30,),
+        SizedBox(
+          width: MediaQuery.of(context).size.width * 0.7,
+          child: PlatformTextFormField(
+            controller: widget.passwordController,
+            material: (_, __) => MaterialTextFormFieldData(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(20), borderSide: BorderSide(color: CustomThemeData.primaryColor)),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: CustomThemeData.primaryColor),
+                ),
+                labelText: "Şifre",
+                prefixIcon: const Icon(Icons.security),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    widget.passwordVisible ? Icons.visibility : Icons.visibility_off,
+                    color: CustomThemeData.accentColor,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      widget.passwordVisible = !widget.passwordVisible;
+                    });
+                  },
+                ),
+              ),
+            ),
+            cupertino: (_, __) => CupertinoTextFormFieldData(
+              decoration: BoxDecoration(
+                border: Border.all(color: CustomThemeData.primaryColor,),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              prefix: Row(
+                children: [
+                  Icon(Icons.security),
+                  IconButton(
+                    icon: Icon(
+                      widget.passwordVisible ? Icons.visibility : Icons.visibility_off,
+                      color: CustomThemeData.accentColor,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        widget.passwordVisible = !widget.passwordVisible;
+                      });
+                    },
+                  ),
+                ],
+              )
+
+            ),
+            autocorrect: false,
+            obscureText: !widget.passwordVisible,
+            validator: (value){
+              if(value == null || value.isEmpty){
+                return "Şifreyi boş bırakmayın";
+              }else{
+                if(RegexUtil.passwordRegex.hasMatch(value)){
+                  return null;
+                }else{
+                  return "Şifreniz yeterince güçlü değil";
+                }
+                }
+            }
+          ),
+        ),
+      ],
+    );
+  }
+}
